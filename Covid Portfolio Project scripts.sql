5d77ec27b383a9c66aa1f60ed1c1bae5fe68c6eb
Angel <97252092+angelruan@users.noreply.github.com>
2022-03-09 21:14:34 -0700

    Update Covid Portfolio Project scripts.sql

--- Covid Portfolio Project scripts.sql
+++ Covid Portfolio Project scripts.sql
@@ -1,64 +1,57 @@
--- Welcome to my very first Data Analyst Portfolio Project using SQL doing data exploratin 
--- In this first exploration project, I will be digging into the covid deaths and covid vaccination and looking at the vacciation percentage, global death rate and etc. 
-/* 
+/*
+Covid 19 Data Exploration 
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 */
-
-select * 
-from 
-	--[Portfolio 1].[dbo].[CovidDeaths$]
-order 3,4 
-
-
---select * 
---from 
---	dbo.CovidVaccinations
---order by 3,4
 
 -- Select Data that We are going to use 
 
 select 
 	location, date, total_cases, new_cases, total_deaths, population
 from 
-	[Portfolio 1].[dbo].[CovidDeaths$]
+	CovidDeaths
 order by 
 	1,2
 
 
 -- Looking at Total Cases vs Total Deaths 
 -- Shows likelihood of dying if you got covid in your country 
+
 select 
 	location, date, total_cases, total_deaths,
-	Format((Total_deaths/total_cases),'P') as percentage_of_death
-from 
-	[Portfolio 1].[dbo].[CovidDeaths$]
-where 
-	location = 'United States'
+	concat(round(cast(total_deaths as float)/cast(total_cases as float) *100,3),'%') as percentage_of_death
+FROM
+    CovidDeaths
+--where 
+    --location ='United States'
 order by 
 	1,2
+
+
 
 -- looking at Total Cases vs Population 
 -- See what percentage of poplucation got covid 
 select 
 	location, date, total_cases, population,
-	Format((total_cases/population),'P1') as percentage_of_diagnosed_covid
+	concat(round(cast(total_cases as float)/cast(population as float) *100,3),'%') as percentage_of_diagnosed_covid
 from 
-	[Portfolio 1].[dbo].[CovidDeaths$]
-where 
-	location = 'United States'
+	CovidDeaths
+--WHERE 
+	--location = 'United States'
 order by 
 	1,2
+
 
 
 -- looking at countries with highest infection rate compared to population 
 select 
 	location, population, max(total_cases) as highest_infection,
-	max((total_cases/population)*100) as percentage_of_population_infected
+	round(max((total_cases/population)*100), 3 )as percentage_of_population_infected
 from 
-	[Portfolio 1].[dbo].[CovidDeaths$]
+	CovidDeaths
 group by location, population 
 order by 
 	4 desc
+
 
 
 -- let's break it down to continets from here 
@@ -68,10 +61,11 @@ order by
 select 
 	continent, max(cast(total_deaths as int)) as total_deaths
 from 
-	[Portfolio 1].[dbo].[CovidDeaths$]
+	CovidDeaths
 where continent is not NULL
 group by continent
 order by 2 desc
+
 
 
 -- GLOBAL NUMBERS 
@@ -79,34 +73,35 @@ order by 2 desc
 select 
 	 date, sum(new_cases) as total_cases,
 	 sum(cast(new_deaths as int)) as total_deaths,
-	 format(sum(cast(new_deaths as int))/sum(new_cases),'P2') as global_death_percentage
+	 round(sum(cast(new_deaths as float))/sum(cast(new_cases as float))*100, 3) as global_death_percentage
 from 
-	[Portfolio 1].[dbo].[CovidDeaths$]
+	CovidDeaths
 where 
 	--location = 'United States'
-continent is not NULL 
+continent IS not NULL 
 group by date
 order by 
 	1,2
 
 
+
 --Looking at total population vs vaccinations 
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
 
 select 
 	covid_death.continent,covid_death.location, covid_death.date, covid_death.population,covid_vaccination.new_vaccinations,
 	sum(cast(covid_vaccination.new_vaccinations as bigint)) 
 	over (partition by covid_death.location order by covid_death.location,covid_death.date) as rolling_people_vac
-	, max(rolling_people_vac)
 from
-	[Portfolio 1].[dbo].[CovidDeaths$] as covid_death
-join [Portfolio 1].[dbo].[CovidVaccinations] as covid_vaccination 
+	CovidDeaths as covid_death
+join CovidVaccinations as covid_vaccination 
 	on covid_death.location = covid_vaccination.location 
 	and covid_death.date = covid_vaccination.date
 where covid_death.continent is not NULL
 order by 2,3
 
 
--- CTE
+-- Using CTE to perform Calculation on Partition By in previous query
 
 with PopvsVac(Continent,location, date, population, new_vaccinations,rolling_people_vac)
 as
@@ -116,13 +111,29 @@ select
 	sum(cast(covid_vaccination.new_vaccinations as bigint)) 
 	over (partition by covid_death.location order by covid_death.location,covid_death.date) as rolling_people_vac
 from
-	[Portfolio 1].[dbo].[CovidDeaths$] as covid_death
-join [Portfolio 1].[dbo].[CovidVaccinations] as covid_vaccination 
+	CovidDeaths as covid_death
+join CovidVaccinations as covid_vaccination 
 	on covid_death.location = covid_vaccination.location 
 	and covid_death.date = covid_vaccination.date
 where covid_death.continent is not NULL
 --order by 2,3
 )
 
-select * , format((rolling_people_vac/population), 'P2')as vac_percet
+select * , concat((rolling_people_vac/population)*100,'%') as vac_percet
 from PopvsVac
+
+
+-- Creating View to store data for later visualizations
+
+Create VIEW PercentPopulationVaccinated as
+Select 
+	covid_death.continent,covid_death.location, covid_death.date, covid_death.population,covid_vaccination.new_vaccinations, 
+    sum(cast(covid_vaccination.new_vaccinations as bigint)) 
+	over (partition by covid_death.location order by covid_death.location,covid_death.date) as rolling_people_vac
+    --, (RollingPeopleVaccinated/population)*100
+from
+	CovidDeaths as covid_death
+join CovidVaccinations as covid_vaccination 
+	on covid_death.location = covid_vaccination.location 
+	and covid_death.date = covid_vaccination.date
+where covid_death.continent is not null 
